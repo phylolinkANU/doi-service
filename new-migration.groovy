375cdfa7db74912ccf6f07f483f01f1ce4719078
Simon Bear <simon.bear@csiro.au>
2017-11-06 15:32:38 +1100

    Update new-migration.groovy to not require it to be run from the same directory

--- new-migration.groovy
+++ new-migration.groovy
@@ -1,10 +1,11 @@
 #!/usr/bin/env groovy
 import java.text.*
+@groovy.transform.SourceURI def sourceURI
 def date = new Date()
 def df = new SimpleDateFormat('yyyyMMddHHmmssSSSS')
 def ts = df.format(date)
 def name = args.length > 0 ? args[0] : "RENAME_ME"
-def file = new File("src/main/resources/db/migration/V${ts}__${name}.sql")
+def file = new File(new File(sourceURI).parentFile,"src/main/resources/db/migration/V${ts}__${name}.sql")
 if (file.createNewFile()) {
   file.withWriter { fw ->
     fw.write('/*\n')
@@ -12,6 +13,7 @@ if (file.createNewFile()) {
     fw.write('See: http://www.jeremyjarrell.com/using-flyway-db-with-distributed-version-control/\n')
     fw.write('*/')
   }
+  println "Created $file"
   return 0
 } else {
   System.err.println("$file already exists!?!?")
